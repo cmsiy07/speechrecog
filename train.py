2ffb4b6b37153579e846f4c47d14192db011d0cf
Cheryl Siy <cherylmsiy@gmail.com>
2023-05-22 00:20:19 +0900

    test

--- train.py
+++ train.py
@@ -282,9 +282,9 @@ def process_eval(model,data_path,data_list,index2char,save_path=None):
         # read the wav file and convert to PyTorch format
         audio, sample_rate = soundfile.read(os.path.join(data_path, file['file']))
         # < fill your code here >
+        x = torch.tensor(audio).unsqueeze(0) 
 
-        # load x and y 
-        x = audio
+
 
 
         # forward pass through the model
